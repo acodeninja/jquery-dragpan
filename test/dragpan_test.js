6080c9aef2f6f691fff298c1d73d02b7e010f102
Lawrence Goldstien <lawrence.goldstien@symphony-group.co.uk>
2013-08-22 16:03:56 +0100

    Wrote test set and included jquery-ui in tests

--- test/dragpan_test.js
+++ test/dragpan_test.js
@@ -20,21 +20,57 @@
       throws(block, [expected], [message])
   */
 
-  module('jQuery#dragpan', {
+  module('Generic', {
     // This will run before each test in this module.
     setup: function() {
       this.elems = $('#parent').dragpan();
     }
   });
 
-  test('is a function', function() {
+  test('Dragpan is a function of $.fn', function() {
     expect(1);
     strictEqual(typeof $.fn.dragpan, 'function', 'dragpan should be a function');
   });
 
-  test('applies cursor', function() {
+  test('Dragpan applies all-scroll cursor', function() {
     expect(1);
-    strictEqual($('#parent').css('cursor'), 'all-scroll', 'the parent should have an all-scroll cursor');
+    strictEqual($('#parent').css('cursor'), 'all-scroll', 'the parent should have an all-scroll cursor by default');
   });
+
+  module('Dragpan On', {
+    setup: function () {
+      this.elems = $('#parent').dragpan();
+      $('#parent').dragpan('on');
+    }
+  });
+
+  test('Dragpan applies all-scroll cursor', function() {
+    expect(1);
+    strictEqual($('#parent').css('cursor'), 'all-scroll', 'the parent should have an all-scroll cursor by default');
+  });
+
+  module('Dragpan With Options', {
+    setup: function () {
+      this.elems = $('#parent').dragpan({ cursor: 'pointer', speedX: 50, speedY: 60 });
+    }
+  });
+
+  test('Drapan uses specified cursor', function() {
+    expect(1);
+    strictEqual($('#parent').css('cursor'), 'pointer', 'the parent should have a pointer cursor for this test');
+  });
+
+  module('Dragpan Off', {
+    setup: function () {
+      this.elems = $('#parent').dragpan();
+      $('#parent').dragpan('off');
+    }
+  });
+
+  test('defaul cursor', function() {
+    expect(1);
+    strictEqual($('#parent').css('cursor'), 'default', 'the parent should have a pointer cursor for this test');
+  });
+
 
 }(jQuery));
